df57d4f20a650278398064622ba01f1e2b75fa34
unknown <i.ksk0601@gmail.com>
2019-10-30 02:41:01 +0900

    多分これがvolume24hだと思うんですけど(名推理)

--- bitmax.py
+++ bitmax.py
@@ -1,10 +1,26 @@
 import requests
+import datetime
+from pytz import timezone
 
-url = 'https://www.bitmex.com/api/v1/instrument'
-params = {
-        'symbol': 'XBTUSD',
-        'startTime': "2019-10-13T12:00:00.000Z"
-        }
-response = requests.get(url, params=params)
-print(response)
-print(response.text)
+
+def main():
+    url = 'https://www.bitmex.com/api/v1/trade/bucketed'
+    params = {
+            'binSize': '1d',
+            'symbol': 'XBT',
+            'startTime': datetime.datetime(2019, 10, 1),
+            'endTime': datetime.datetime.now(timezone('UTC'))
+            }
+
+    response = requests.get(url, params=params)
+    print(response)
+    print(response.url)
+    print(len(response.json()))
+    for info in response.json():
+        print(info['timestamp'])
+        print(info['volume'])
+        print('-------------------------------------')
+
+
+if __name__ == "__main__":
+    main()
